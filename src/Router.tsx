76993c6889d8eb4bcd56abb23df47010dc53b77d
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-08-03 22:07:15 +0530

    Setup Routes using React Router DOM

---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes } from "react-router-dom";
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Authentication />}></Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
